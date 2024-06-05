8fe4a7e6612d8bc387ed33ba511eca62eb485b5c
teteulf <theolefevre791@gmail.com>
2024-06-05 17:20:20 -0300

    component about made

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { IoSchool } from "react-icons/io5";
 import {
   AboutInfo,
   EducationInfo,
@@ -13,36 +14,77 @@ const aboutData = [
   { name: "Experience", info: <ExperienceInfo /> },
 ];
 
-export default function about() {
+export default function About() {
   const [index, setIndex] = useState(0);
   return (
-    <main className="flex w-full h-full items-center justify-center">
-      <section>
-        <h1></h1>
-        <p></p>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+    <main className="flex w-full h-full items-center justify-center gap-12 ">
+      <section className="flex flex-col">
+        <h1 className="text-[60px] text-white font-bold">
+          Captivating <strong className="text-red-600">stories</strong>.
+        </h1>
+        <p className="w-[550px] opacity-50 text-white text-[14px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quia
+          enim voluptate perferendis. Illum recusandae perferendis officiis
+          autem maxime repellat delectus labore sequi. Veniam repellendus cumque
+          placeat dolor vero perspiciatis.
+        </p>
+        <div className="flex mt-20 items-center gap-6">
+          <div className="flex flex-col justify-center border-r-[0.5px] border-gray-500 border-opacity-50 max-w-[130px] max-h-[110px] pr-10">
+            <div className="text-[35px] text-red-600 font-extrabold">10+</div>
+            <p className="text-white max-w-[100px] uppercase text-xs tracking-[1px] leading-[1.4]">
+              technologies that I work
+            </p>
+          </div>
+          <div className="flex flex-col justify-center border-r-[0.5px] border-gray-500 border-opacity-50 max-w-[130px] max-h-[110px] pr-10">
+            <div className="text-[35px] text-red-600 font-extrabold">15+</div>
+            <p className="text-white max-w-[100px] uppercase text-xs tracking-[1px] leading-[1.4]">
+              course certificates
+            </p>
+          </div>
+          <div className="flex flex-col justify-center border-r-[0.5px] border-gray-500 border-opacity-50 max-w-[130px] max-h-[110px] pr-10">
+            <div className="text-[35px] text-red-600 font-extrabold">150+</div>
+            <p className="text-white max-w-[100px] uppercase text-xs tracking-[1px] leading-[1.4]">
+              hours of course
+            </p>
+          </div>
+          <div className="flex flex-col justify-center max-w-[130px] max-h-[110px]">
+            <div className="text-[35px] text-red-600 font-extrabold">4+</div>
+            <p className="text-white max-w-[100px] uppercase text-xs tracking-[1px] leading-[1.4]">
+              projects completed
+            </p>
+          </div>
         </div>
       </section>
-      <section className="flex flex-col">
-        <div className="flex gap-8 ">
+      <section className="flex flex-col items-start justify-center relative gap-8 min-w-[412px]">
+        <div className="flex gap-12">
           {aboutData.map((item, itemIndex) => (
             <div
               className={`${
-                index === itemIndex &&
-                "text-red-600 after:bg-red-600 after:transition-all after:duration-300 after:w-full"
-              } text-white relative after:w-8 after:h-[2px] after:absolute after:bg-white after:bottom-0 after:left-0 cursor-pointer`}
+                index === itemIndex ? "text-red-600" : "text-white"
+              } relative cursor-pointer`}
               key={itemIndex}
               onClick={() => setIndex(itemIndex)}
             >
               {item.name}
+              <div
+                className={`${
+                  index === itemIndex &&
+                  "bg-red-600 transition-all w-[100%] duration-500"
+                } w-8 h-[2px] absolute bg-white bottom-0 left-0`}
+              ></div>
             </div>
           ))}
         </div>
-        <div className="text-white">{aboutData[index].info}</div>
+        <div className="flex text-white relative min-h-[150px] min-w-[300px] ">
+          {aboutData[index].info}
+        </div>
+        <a className="flex flex-start w-[400px] items-center text-[20px] text-gray-400 cursor-pointer hover:text-gray-600 group">
+          Download my CV -{" "}
+          <IoSchool
+            className="ml-2 text-gray-400 group-hover:text-gray-600"
+            size={30}
+          />{" "}
+        </a>
       </section>
     </main>
   );
--- app/components/aboutInfo.jsx
+++ app/components/aboutInfo.jsx
@@ -1,19 +1,84 @@
+"use-client";
+
 import { IoLogoJavascript, IoLogoCss3, IoLogoReact } from "react-icons/io5";
 import { FaHtml5, FaGithub, FaGitAlt, FaNodeJs } from "react-icons/fa";
 import { SiTailwindcss, SiStyledcomponents, SiFramer } from "react-icons/si";
 import { RiNextjsFill } from "react-icons/ri";
 import { BiLogoTypescript } from "react-icons/bi";
+import { useState } from "react";
+
+const iconsData = [
+  { icon: <IoLogoJavascript size={25} />, popUp: "JavaScript" },
+  { icon: <IoLogoCss3 size={25} />, popUp: "Css" },
+  { icon: <FaHtml5 size={25} />, popUp: "Html" },
+  { icon: <IoLogoReact size={25} />, popUp: "React , ReactNative" },
+  { icon: <SiTailwindcss size={25} />, popUp: "TailWindCss" },
+  { icon: <SiStyledcomponents size={25} />, popUp: "SyledComponents" },
+  { icon: <FaGithub size={25} />, popUp: "GitHub" },
+  { icon: <FaGitAlt size={25} />, popUp: "Git" },
+  { icon: <FaNodeJs size={25} />, popUp: "NodeJs" },
+  { icon: <SiFramer size={25} />, popUp: "Framer-motion" },
+  { icon: <RiNextjsFill size={25} />, popUp: "NextJs" },
+  { icon: <BiLogoTypescript size={25} />, popUp: "typeScript" },
+];
 
 export function AboutInfo() {
+  const [index, setIndex] = useState(null);
+  const firstSixIcons = iconsData.slice(0, 6);
+  const remainingIcons = iconsData.slice(6);
   return (
     <>
       <div>
-        <div className="flex items-center justify-center">
-          {" "}
-          Web development : <IoLogoJavascript /> <IoLogoCss3 /> <FaHtml5 />{" "}
-          <IoLogoReact /> <SiTailwindcss /> <SiStyledcomponents /> <FaGithub />{" "}
-          <FaGitAlt /> <FaNodeJs /> <SiFramer /> <RiNextjsFill />{" "}
-          <BiLogoTypescript />
+        <div className="flex items-center justify-center text-[18px] gap-4">
+          <div className="opacity-50">Web development -</div>
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-4">
+              {firstSixIcons.map((item, itemIndex) => (
+                <div
+                  key={itemIndex}
+                  className="cursor-pointer flex flex-col items-center justify-center"
+                  onMouseEnter={() => setIndex(itemIndex)}
+                  onMouseLeave={() => setIndex(null)}
+                >
+                  {item.icon}
+                  <div
+                    className={`${
+                      itemIndex === index ? "visible" : "hidden"
+                    } absolute bg-white text-black -mt-16 rounded-[4px] px-2 opacity-60`}
+                    style={{ whiteSpace: "nowrap" }}
+                  >
+                    {item.popUp}
+                    <span
+                      className={`absolute w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-t-[6px] border-t-white left-1/2 transform -translate-x-1/2 -bottom-[6px]`}
+                    ></span>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="flex gap-4">
+              {remainingIcons.map((item, itemIndex) => (
+                <div
+                  key={itemIndex}
+                  className="flex flex-col items-center justify-center cursor-pointer"
+                  onMouseEnter={() => setIndex(itemIndex + 6)}
+                  onMouseLeave={() => setIndex(null)}
+                >
+                  {item.icon}
+                  <div
+                    className={`${
+                      itemIndex + 6 === index ? "visible" : "hidden"
+                    } absolute bg-white text-black mt-20 rounded-[4px] px-2 opacity-60`}
+                    style={{ whiteSpace: "nowrap" }}
+                  >
+                    {item.popUp}
+                    <span
+                      className={`absolute w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[6px] border-b-white left-1/2 transform -translate-x-1/2 -top-[6px]`}
+                    ></span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         <div></div>
       </div>
@@ -22,9 +87,9 @@ export function AboutInfo() {
 }
 
 export function EducationInfo() {
-  return <div> aasdasdasdasdasd</div>;
+  return <div className="text-[18px]"> aasdasdasdasdasdasdasdasdasdasdas</div>;
 }
 
 export function ExperienceInfo() {
-  return <div>asddasdasdadasd</div>;
+  return <div className="text-[18px]">asddasdasdadasdasdasdasdasdasdass</div>;
 }
