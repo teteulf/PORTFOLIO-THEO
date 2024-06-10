082011a355419eefb3df4d7f556b9616096ceaab
teteulf <theolefevre791@gmail.com>
2024-06-10 17:27:19 -0300

    creatind carrousel on projects page.

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -14,31 +14,35 @@ import "swiper/css/scrollbar";
 
 export default function Projects() {
   return (
-    <main className="text-white flex w-full max-w-[80%] h-full items-center justify-center">
-      <section className="flex flex-col items-center flex-initial mt-0">
+    <main className="text-white flex w-full h-full items-center justify-center">
+      <section className="flex flex-col items-center flex-initial mt-0 w-[550px]">
         <h1 className="font-bold text-[45px]">
           My Projects<strong className="text-[45px] text-red-600">.</strong>
         </h1>
-        <p className="w-[50%]">
+        <p className="">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed
           voluptatibus omnis corrupti quia accusantium error magni distinctio
           eos, rerum explicabo labore eveniet beatae, minima magnam, pariatur
           impedit ullam velit veniam.
         </p>
       </section>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar]}
-        slidesPerView={1}
-        navigation
-        className="w-[50%]"
-      >
-        <section className="flex gap-2">
+
+      <section className="">
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar]}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          navigation
+          className="w-[380px] flex items-center justify-center"
+          id="swiper "
+        >
           <SwiperSlide>
-            <Link
-              href="https://api-movies-git-main-teteulfs-projects.vercel.app/"
-              target="_blank"
-            >
-              <div className="bg-[#24246046] overflow-hidden hover:bg-[#40409646] rounded-lg h-[350px] w-[250px] relative flex flex-col items-start justify-center cursor-pointer group">
+            <div className="bg-[#24246046] hover:bg-[#40409646] rounded-lg h-[350px] w-[250px] relative flex flex-col ml-16  items-start justify-center cursor-pointer group">
+              <Link
+                href="https://api-movies-git-main-teteulfs-projects.vercel.app/"
+                target="_blank"
+                className=""
+              >
                 <div className="flex flex-col gap-16 -mt-12">
                   <h1 className="ml-12 flex items-center gap-2">
                     <BiCameraMovie size={30} className="text-red-600" /> Movie
@@ -61,14 +65,18 @@ export default function Projects() {
                     className="ml-10 -rotate-45 group-hover:rotate-[40px] group-hover:text-red-600 transition duration-200"
                   />
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="bg-[#2d2d78] opacity-20 ml-16 rounded-lg h-[350px] w-[250px]  "></div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className="bg-[#2d2d78] opacity-20 rounded-lg h-[350px] w-[250px]"></div>
+            <div className="bg-[#2d2d78] opacity-20 ml-16 rounded-lg h-[350px] w-[250px] "></div>
           </SwiperSlide>
-        </section>
-      </Swiper>
+        </Swiper>
+      </section>
     </main>
   );
 }
